e4de15ba63090d896450e8b6ceb5e8e7f9402bfd
Semih Şahinoğlu <semihshngl@gmail.com>
2025-07-19 21:39:36 +0300

    anaekrana tarih eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import TaskList from "./components/TaskList";
 function App() {
   return (
     <div className="w-max flex flex-col gap-5 mx-auto mt-[10%] bg-[#111] rounded-2xl py-8 px-6 h-[500px] max-h-[500px]">
-      <h1 className="font-bold text-3xl text-white">To-Do List 📋</h1>
+      <div
+        className="flex
+      "
+      >
+        <h1 className="font-bold text-3xl text-white">To-Do List 📋</h1>
+        <span className="ml-auto font-bold text-2xl text-white">
+          {new Date().toLocaleDateString()}
+        </span>
+      </div>
       <AddTask />
       <TaskList />
     </div>
